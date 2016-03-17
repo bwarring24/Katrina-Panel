2ebe36a3cf925074d88b8acb044be1853b10c01c
Blake Warrington <bwarring24@gmail.com>
2016-03-17 13:37:59 -0400

    Added colors and also relocated some theme assets

--- pages/headers/register.head.inc.php
+++ pages/headers/register.head.inc.php
@@ -18,16 +18,16 @@
         $sql = "SELECT * FROM users WHERE email='".$email."'";
         $DB->query($sql);
 
-        //echo $DB->rowCount();
         if($DB->rowCount() >= 1){
             $validated = false;
+            echo "<span class='error'>That email has already been registered!</span>";
         }
         
         if($validated){
             $sql = "INSERT INTO users (firstname, lastname, password, email) VALUES ('$firstName', '$lastName','$password', '$email')";
             
             if ($DB->query($sql)) {
-                echo "Successful Registration!";
+                echo "<span class='success'>Successful Registration!</span>";
             } else {
                 echo "Error: " . $sql . "<br>" . $DB->error();
             }
