cb1e33bbd977fc291136d57c4abc4bbbf541a744
Blake Warrington <bwarring24@gmail.com>
2016-03-17 23:04:15 -0400

    changed the placeholder text for fun

--- pages/register.pg.php
+++ pages/register.pg.php
@@ -8,22 +8,22 @@
 
   <div id="login">
     <div class="frmLabel"><h2>{lang:register-firstName}</h2></div>
-    <div class="frmInput"><h2><input id="txtFirstName" type="text" name="firstName" pattern="^[a-zA-Z]+$" placeholder="i.e. John" oninput="check(this)" size="20" required /></h2></div>
+    <div class="frmInput"><h2><input id="txtFirstName" type="text" name="firstName" pattern="^[a-zA-Z]+$" placeholder="i.e. Bill" oninput="check(this)" size="20" required /></h2></div>
   </div>
 
   <div id="login">
     <div class="frmLabel"><h2>{lang:register-lastName}</h2></div>
-    <div class="frmInput"><h2><input id="txtLastName" type="text" name="lastName" pattern="^[a-zA-Z]+$" placeholder="i.e. Doe" oninput="check(this)" size="20" required/></h2></div>
+    <div class="frmInput"><h2><input id="txtLastName" type="text" name="lastName" pattern="^[a-zA-Z]+$" placeholder="i.e. Gates" oninput="check(this)" size="20" required/></h2></div>
   </div>
 
   <div id="login">
     <div class="frmLabel"><h2>{lang:register-email}</h2></div>
-    <div class="frmInput"><h2><input id="txtEmail" type="email" name="email" placeholder="johndoe@gmail.com" size="20" required/></h2></div>
+    <div class="frmInput"><h2><input id="txtEmail" type="email" name="email" placeholder="billgates@microsoft.com" size="20" required/></h2></div>
   </div>
 
   <div id="login">
     <div class="frmLabel"><h2>{lang:login-password}</h2></div>
-    <div class="frmInput"><h2><input id="txtPass" type="password" name="password" pattern="^(?=.*[A-Za-z\W])(?=.*\d)[A-Za-z\d]{5,}$" placeholder="i.e. abcdef123" oninput="check(this)" size="20" required/></h2></div>
+    <div class="frmInput"><h2><input id="txtPass" type="password" name="password" pattern="^(?=.*[A-Za-z\W])(?=.*\d)[A-Za-z\d]{5,}$" placeholder="i.e. I<3Microsoft" oninput="check(this)" size="20" required/></h2></div>
   </div>
 
   <div id="line">
